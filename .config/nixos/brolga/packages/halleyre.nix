36a39f73ed1a1ed9baae881da01f27d3e5749c10
argo <argo>
2026-03-18 04:53:46 +0000

    argo12

--- .config/nixos/brolga/packages/halleyre.nix
+++ .config/nixos/brolga/packages/halleyre.nix
@@ -1,13 +1,14 @@
 { pkgs, ... }:
 
 {
-  users.users.halleyre.packages = with pkgs; [
+  users.users.argonaut12.packages = with pkgs; [
     # terminal
     radare2
 
     # ui
     nautilus # stupid ass gtk filechooser (check out termchooser sometime to utilise yazi)
     (catppuccin-gtk.override { variant = "mocha"; })
+    gitkraken
 
     # language servers
     clang-tools
